0215fa1e69679c95b6c3b702f1989bf0aed0d488
Tristan Hancock <tristanhancock@gmail.com>
2025-07-28 23:11:04 +0530

    added icon for chat

--- src/components/icons/OveliaIcon.tsx
+++ src/components/icons/OveliaIcon.tsx
@@ -1,10 +1,25 @@
-
+// src/components/icons/OveliaIcon.tsx
 import React from 'react';
+import oveliaImg from '../../assets/ovelialogo.jpeg';
 
-const OveliaIcon: React.FC = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#6B66FF]" viewBox="0 0 24 24" fill="currentColor">
-        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-12h2v2h-2v-2zm0 4h2v6h-2v-6z"/>
-    </svg>
+export interface OveliaIconProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  /**  
+   * You can also add a `size` prop if you’d rather:  
+   * size?: number;  
+   * then spread `width={size} height={size}` onto the <img>  
+   */
+}
+
+const OveliaIcon: React.FC<OveliaIconProps> = ({
+  className = '',
+  ...imgProps
+}) => (
+  <img
+    src={oveliaImg}
+    alt="Ovelia logo"
+    className={`object-contain ${className}`}
+    {...imgProps}
+  />
 );
 
 export default OveliaIcon;
